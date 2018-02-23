7d117b8186a1c5f00b6ffb07e0518fab03829c75
azlinjalal <azlin.jalal@moe.gov.my>
2018-02-23 10:53:43 +0800

    Add files via upload

---
+++ Cachematrix.R
@@ -0,0 +1,22 @@
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(inverse) inv <<- inverse
+  get_inv <- function() inv
+  list(set=set, get=get, set_inv=set_inv, get_inv=get_inv)
+}
+cacheSolve <- function(x, ...) {
+  inv <- x$get_inv()
+  if(!is.null(inv)) {
+    message("getting cache")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$set_inv(inv)
+  inv
+}
